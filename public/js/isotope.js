15ad23d2a2f9ea6a9a016b38e977359c017bb919
Alex <47950293+Snowryder96@users.noreply.github.com>
2021-01-19 23:38:16 +0100

    .

--- public/js/isotope.js
+++ public/js/isotope.js
@@ -1,13 +1,8 @@
 const $grid = $('.grid').isotope({
-  // options
   itemSelector: '.grid-item',
   layoutMode: 'masonry',
 });
 
-// $('#category').on('click', 'button', function(){
-//   $('#category button').removeClass("active");
-//   $(this).addClass("active")
-// });
 var filters = {}
 
 function concatValues( obj ) {
@@ -43,53 +38,8 @@ $('.filter-button-group').each( function( i, buttonGroup ) {
 });
 
 
-// $('#category #technic #availability').on( 'click', 'button', function() {
-//   var $this = $(this);
-//   // get group key
-//   var $buttonGroup = $this.parents('.button-group');
-//   var filterGroup = $buttonGroup.attr('data-filter-group');
-//   // set filter for group
-//   filters[ filterGroup ] = $this.attr('data-filter');
-//   // combine filters
-//   var filterValue = concatValues( filters );
-//   $grid.isotope({ filter: filterValue });
-//   console.log(filters)
-// });
 
 
 
-
-// $('#technic').on('click', 'button', function(){
-//   $('#technic button').removeClass("active");
-//   $(this).addClass("active")
-// });
-// $('#technic').on( 'click', 'button', function() {
-//   var $this = $(this);
-//   // get group key
-//   var $buttonGroup = $this.parents('.button-group');
-//   var filterGroup = $buttonGroup.attr('data-filter-group');
-//   // set filter for group
-//   filters[ filterGroup ] = $this.attr('data-filter');
-//   // combine filters
-//   var filterValue = concatValues( filters );
-//   $grid.isotope({ filter: filterValue });
-//   console.log(filters)
-// });
-// $('#availability').on('click', 'button', function(){
-//   $('#availability button').removeClass("active");
-//   $(this).addClass("active")
-// });
-// $('#availability').on( 'click', 'button', function() {
-//   var $this = $(this);
-//   // get group key
-//   var $buttonGroup = $this.parents('.button-group');
-//   var filterGroup = $buttonGroup.attr('data-filter-group');
-//   // set filter for group
-//   filters[ filterGroup ] = $this.attr('data-filter');
-//   // combine filters
-//   var filterValue = concatValues( filters );
-//   $grid.isotope({ filter: filterValue });
-//   console.log(filters)
-// });
 
 
